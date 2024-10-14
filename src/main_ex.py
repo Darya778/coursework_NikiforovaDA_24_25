5c413dd4bf2892cfce26979e502da72e19ec4f91
Darya778 <112837727+Darya778@users.noreply.github.com>
2024-10-14 13:47:56 +0800

    Update main_ex.py (add db "example")

--- src/main_ex.py
+++ src/main_ex.py
@@ -1,24 +1,26 @@
-  from flask import Flask, request, jsonify
-  from flask_cors import CORS
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
 
-  app = Flask(__name__)
-  CORS(app)
+app = Flask(__name__)
+CORS(app)
 
-  # DB
-  data_store = {}
+db = mysql.connector.connect(user='dasha', password='1qaz@WSX12',
+                              host='127.0.0.1',
+                              database='example')
 
-  @app.route('/send_data', methods=['POST'])
-  def send_data():
-      user_id = request.json.get('user_id')
-      data = request.json.get('data')
-      data_store[user_id] = data
-      return jsonify({'status': 'success'}), 200
+@app.route('/send_data', methods=['POST'])
+def send_data():
+    user_id = request.json.get('user_id')
+    data = request.json.get('data')
+    db[user_id] = data
+    return jsonify({'status': 'success'}), 200
 
-  @app.route('/get_data/<user_id>', methods=['GET'])
-  def get_data(user_id):
-      data = data_store.get(user_id)
-      return jsonify({'data': data}), 200 if data else (404, 'Data not found')
+@app.route('/get_data/<user_id>', methods=['GET'])
+def get_data(user_id):
+    data = db.get(user_id)
+    return jsonify({'data': data}), 200 if data else (404, 'Data not found')
 
-  if __name__ == '__main__':
-      app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
   
